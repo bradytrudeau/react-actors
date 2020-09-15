006c16cda26332a08453fae589c591cb78dd5b37
Brady Trudeau <bradytrudeau@gmail.com>
2020-09-15 17:11:34 -0500

    React Imported to each src file and file export

---
+++ src/Contact/Contact.js
@@ -0,0 +1,15 @@
+// Import React library
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+// Export this file to the app
+export default Contact;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,15 @@
+// Import React library
+import React, { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+// Export this file to the app
+export default Footer;
